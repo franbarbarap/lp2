460d468fb30f9eb23c5885dbc4d213a406be3c9a
Francielli Barbara <franbarbarap@gmail.com>
2017-06-13 14:06:34 -0300

    Fechando as conexoes com o banco de dados

--- src/dao/JDBCExampleDAO.java
+++ src/dao/JDBCExampleDAO.java
@@ -19,7 +19,7 @@ public class JDBCExampleDAO implements ExampleDAO{
 	private Connection conn;
 	
 	public JDBCExampleDAO() {
-		this.conn = ConnectionFactory.getConnection();
+		
 	}
 
 	/**
@@ -30,14 +30,29 @@ public class JDBCExampleDAO implements ExampleDAO{
 	 */
 	@Override
 	public void save(Example example) {
+		this.conn = ConnectionFactory.getConnection();
 		String sql = "INSERT INTO example (date) VALUES (?);";
+		PreparedStatement stmt = null;
 		try {
-			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt = conn.prepareStatement(sql);
 			stmt.setTimestamp(1, Utils.toTimestamp(example.getDate()));
 			stmt.execute();
-			stmt.close();
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
+		} finally {
+			try {
+				if(stmt != null){
+					stmt.close();
+				}
+			} catch (SQLException e) {
+				throw new RuntimeException(e);
+			}finally {
+				try {
+					this.conn.close();
+				} catch (SQLException e) {
+					throw new RuntimeException(e);
+				}
+			}
 		}
 	}
 
@@ -49,14 +64,29 @@ public class JDBCExampleDAO implements ExampleDAO{
 	 */
 	@Override
 	public void delete(Example example) {
+		this.conn = ConnectionFactory.getConnection();
 		String sql = "DELETE FROM example WHERE id = ?;";
+		PreparedStatement stmt = null;
 		try {
-			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt = conn.prepareStatement(sql);
 			stmt.setLong(1, example.getId());
 			stmt.execute();
-			stmt.close();
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
+		} finally {
+			try {
+				if(stmt != null){
+					stmt.close();
+				}
+			} catch (SQLException e) {
+				throw new RuntimeException(e);
+			}finally {
+				try {
+					this.conn.close();
+				} catch (SQLException e) {
+					throw new RuntimeException(e);
+				}
+			}
 		}
 	}
 
@@ -68,23 +98,43 @@ public class JDBCExampleDAO implements ExampleDAO{
 	 */
 	@Override
 	public ArrayList<Example> list() {
+		this.conn = ConnectionFactory.getConnection();
 		String sql = "SELECT * FROM example;";
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		ArrayList<Example> dates = new ArrayList<>();
 		try {
-			ArrayList<Example> dates = new ArrayList<>();
-			PreparedStatement stmt = conn.prepareStatement(sql);
-			ResultSet rs = stmt.executeQuery();
+			stmt = conn.prepareStatement(sql);
+			rs = stmt.executeQuery();
 			while(rs.next()){
 				Example example = new Example();
 				example.setId(rs.getLong("id"));
 				example.setDate(Utils.toGregorianCalendar(rs.getTimestamp("date")));
 				dates.add(example);
 			}
-			rs.close();
-			stmt.close();
-			return dates;
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
+		} finally {
+			try {
+				if(rs != null){
+					rs.close();
+				}
+
+				if(stmt != null){
+					stmt.close();
+				}
+				
+			} catch (SQLException e) {
+				throw new RuntimeException(e);
+			}finally {
+				try {
+					this.conn.close();
+				} catch (SQLException e) {
+					throw new RuntimeException(e);
+				}
+			}
 		}
+		return dates;
 	}
 	
 	public static void main(String[] args) {
